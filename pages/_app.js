db67185cf88b1201be9736ca12692051a16f5c52
AlienXPredator <ahmads.says@hotmail.com>
2023-11-24 00:22:28 +0500

    project files

---
+++ pages/_app.js
@@ -0,0 +1,38 @@
+import "../styles/globals.css";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { UseWalletProvider } from "use-wallet";
+import NavBar from "../components/Navbar";
+import Footer from "../components/Footer";
+import "@fontsource/space-grotesk";
+
+const theme = extendTheme({
+  fonts: {
+    heading: "Space Grotesk",
+    body: "Space Grotesk",
+  },
+});
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      {" "}
+      <ChakraProvider theme={theme}>
+        <UseWalletProvider
+          chainId={11155111}
+          connectors={{
+            walletconnect: {
+              rpcUrl:
+                // "PLACE HERE",
+            },
+          }}
+        >
+          <NavBar />
+          <Component {...pageProps} />
+          <Footer />{" "}
+        </UseWalletProvider>
+      </ChakraProvider>
+    </>
+  );
+}
+
+export default MyApp;
